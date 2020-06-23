1d6262a27b7d4e7e78af6bf867886bdbf718ec10
Mr.Black <misterblack0@protonmail.com>
2020-06-23 16:27:24 +0300

    add app files

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function clearFilter() {
+  var uncheck = document.getElementsByTagName('input');
+  for (var i = 0; i < uncheck.length; i++) {
+    if (uncheck[i].type == 'checkbox') {
+      uncheck[i].checked = false;
+    }
+  }
+}
